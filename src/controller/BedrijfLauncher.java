336575eb459a11068638081e5a6cd8c9ba17219e
Vincent Velthuizen <vincent@luvistudio.nl>
2024-03-07 09:52:34 +0100

    Dagdeel 2

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -13,15 +13,24 @@ public class BedrijfLauncher {
         System.out.println(Persoon.aantalPersonen);
         Persoon baas = new Persoon("Mark", "Den Haag", 10000);
         System.out.println(Persoon.aantalPersonen);
-        System.out.println(baas.personeelsNummer);
+        System.out.println(baas.getPersoneelsNummer());
         Persoon medewerker = new Persoon("Caroline", "Delft", 4000);
         System.out.println(Persoon.aantalPersonen);
-        System.out.println(medewerker.personeelsNummer);
+        System.out.println(medewerker.getPersoneelsNummer());
         Persoon assistent = new Persoon ("Klaas");
         Persoon manager = new Persoon();
         System.out.println(Persoon.aantalPersonen);
-        System.out.printf("%s verdient %.2f per jaar\n", baas.naam, baas.berekenJaarInkomen());
-        System.out.printf("%s woont in %s\n", assistent.naam, assistent.woonplaats);
+
+        printPersoonsInformatie(baas);
+        printPersoonsInformatie(medewerker);
+        printPersoonsInformatie(assistent);
+        printPersoonsInformatie(manager);
+    }
+
+    public static void printPersoonsInformatie(Persoon persoon) {
+        System.out.printf("%s verdient %.2f en woont in %s en heeft %s recht op een bonus.\n",
+                persoon.getNaam(),persoon.getMaandSalaris(), persoon.getWoonplaats(),
+                persoon.heeftRechtOpBonus() ? "wel" : "geen");
     }
 
 }
--- src/model/Persoon.java
+++ src/model/Persoon.java
@@ -6,35 +6,65 @@ package model;
  * Dit is een persoon betrokken bij de activiteiten van ons bedrijf
  **/
 public class Persoon {
+    public static final double GRENSWAARDE_BONUS = 4500.00;
+
+    private static final String DEFAULT_NAAM = "Onbekend";
+    private static final String DEFAULT_WOONPLAATS = "Onbekend";
+    private static final double DEFAULT_MAAND_SALARIS = 0.0;
+
     public static int aantalPersonen = 0;
 
-    public int personeelsNummer;
-    public String naam;
-    public String woonplaats;
-    public double maandSalaris;
+    private int personeelsNummer;
+    private String naam;
+    private String woonplaats;
+    private double maandSalaris;
 
     public Persoon(String naam, String woonplaats, double maandSalaris) {
         this.naam = naam;
         this.woonplaats = woonplaats;
-        this.maandSalaris = maandSalaris;
+        setMaandSalaris(maandSalaris);
         this.personeelsNummer = ++aantalPersonen;
     }
 
     public Persoon(String naam) {
-        this.naam = naam;
-        this.woonplaats = "Onbekend";
-        this.maandSalaris = 0.0;
-        this.personeelsNummer = ++aantalPersonen;
+        this(naam, DEFAULT_WOONPLAATS, DEFAULT_MAAND_SALARIS);
     }
 
     public Persoon() {
-        this.naam = "Onbekend";
-        this.woonplaats = "Onbekend";
-        this.maandSalaris = 0.0;
-        this.personeelsNummer = ++aantalPersonen;
+        this(DEFAULT_NAAM);
     }
 
     public double berekenJaarInkomen() {
         return 12 * maandSalaris;
+    }
+
+    public boolean heeftRechtOpBonus() {
+        return maandSalaris >= GRENSWAARDE_BONUS;
+    }
+
+    public int getPersoneelsNummer() {
+        return personeelsNummer;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public double getMaandSalaris() {
+        return maandSalaris;
+    }
+
+    private void setMaandSalaris(double hetNieuweMaandsalaris) {
+        if (hetNieuweMaandsalaris < 0) {
+            System.err.println("Het opgegeven maandsalaris is niet geldig, het mag geen negatief getal zijn. " +
+                    "Maandsalaris wordt op 0 gezet");
+            this.maandSalaris = DEFAULT_MAAND_SALARIS;
+        } else {
+            this.maandSalaris = hetNieuweMaandsalaris;
+        }
     }
 }
